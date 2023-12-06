316d8d64e84b02e8082b1b7cdfb1ed7bcb4049a5
Nataliia Smalchenko <137941820+lana-daalang@users.noreply.github.com>
2023-12-06 08:36:53 +0200

    resolved first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const elements = {
+  btnStart: document.querySelector('.js-btn-start'),
+  btnStop: document.querySelector('.js-btn-stop'),
+  body: document.body,
+};
 
+elements.btnStop.disabled = true;
+
+elements.btnStart.addEventListener('click', startColorChange);
+elements.btnStop.addEventListener('click', stopColorChange);
+
+let interval;
+
+function startColorChange() {
+  interval = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  elements.btnStart.disabled = true;
+  elements.btnStop.disabled = false;
+  elements.body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopColorChange() {
+  clearInterval(interval);
+  elements.btnStart.disabled = false;
+  elements.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
